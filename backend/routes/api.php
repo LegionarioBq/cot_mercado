e987b1b30db0b86811c534970de4cf34627fc673
Albert <aandrade.ext@acsp.com.br>
2025-09-25 15:03:11 -0300

    feat: configurando base da app back e front 00

---
+++ backend/routes/api.php
@@ -0,0 +1,8 @@
+use Illuminate\Support\Facades\Route;
+
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'API Laravel está respondendo 🚀'
+    ]);
+});
